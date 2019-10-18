25fed708e769fac3c8f8d17448a0f87f04cd94cb
Anna Ślimak <slimakania@gmail.com>
2019-10-18 13:16:00 +0200

    Initial commit for frontend app

---
+++ frontend/src/Logo.js
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "./logo.svg";
+
+class Logo extends React.Component {
+  render() {
+    return <img src={logo} className="App-logo" alt="logo" />;
+  }
+}
+
+export default Logo;
