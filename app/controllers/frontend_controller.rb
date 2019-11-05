f23cf56ab4c717fb3bfe1885d3cbb334f2f43e1f
Ania Olak <anna.olak@lunarlogic.io>
2019-11-05 13:57:13 +0100

    Fix problem with rendering public/index in development

--- app/controllers/frontend_controller.rb
+++ app/controllers/frontend_controller.rb
@@ -2,6 +2,10 @@
 
 class FrontendController < ApplicationController
   def index
-    render file: 'public/index.html'
+    if Rails.env.production?
+      render file: 'public/index.html'
+    else
+      render plain: "In development please use npm start and visit localhost:3000."
+    end
   end
 end
