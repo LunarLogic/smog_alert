331d365864f168f2e44c5ca67b7647438b78913a
Ania Olak <anna.olak@lunarlogic.io>
2019-11-12 18:12:43 +0100

    Add webpacker and frontend/index view
    Frontend/index view was added to verify that webpacker works.

--- app/controllers/frontend_controller.rb
+++ app/controllers/frontend_controller.rb
@@ -4,8 +4,6 @@ class FrontendController < ApplicationController
   def index
     if Rails.env.production?
       render file: 'public/index.html'
-    else
-      render plain: "In development please use npm start and visit localhost:3000."
     end
   end
 end
