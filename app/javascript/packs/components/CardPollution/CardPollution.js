73ea7b3b3a202576afa63e60f168e3ddeedff4f4
Olga Czarnecka <olga.czarnecka@lunarlogic.io>
2019-11-19 15:58:19 +0100

    Add CardPollution overview component with styles

---
+++ app/javascript/packs/components/CardPollution/CardPollution.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./CardPollution.scss";
+
+const CardPollution = () => {
+  return (
+    <div className="card-pollution__container">
+      <div className="card-pollution__current-data-container">
+        <div className="card-pollution__current-data-overview">
+          <div className="card-pollution__current-data-overview-face"></div>
+          <div className="card-pollution__current-data-overview-text">
+            Niezdrowa
+          </div>
+        </div>
+        <div className="card-pollution__current-data-specific">
+          <div className="card-pollution__current-data-specific-wrapper">
+            <div className="card-pollution__current-data-specific-primary">
+              <div className="card-pollution__current-data-specific-primary-index">
+                PM 10
+              </div>
+              <div className="card-pollution__current-data-specific-primary-value">
+                <span className="card-pollution__current-data-specific-primary-value--bold">
+                  56
+                </span>
+                μg
+              </div>
+            </div>
+            <div className="card-pollution__current-data-specific-secondary">
+              <div className="card-pollution__current-data-specific-secondary-index">
+                PM 2.5
+              </div>
+              <div className="card-pollution__current-data-specific-secondary-value">
+                <span className="card-pollution__current-data-specific-secondary-value--bold">
+                  22
+                </span>
+                μg
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CardPollution;
--- app/javascript/packs/pages/Homepage/Homepage.js
+++ app/javascript/packs/pages/Homepage/Homepage.js
@@ -1,10 +1,23 @@
 import React from "react";
+import "./Homepage.scss";
 import Searchbox from "../../components/Searchbox/Searchbox";
+import CardPollution from "../../components/CardPollution/CardPollution";
+import Town from "../../../../assets/images/Town.png";
 
 const Homepage = () => {
   return (
-    <div className="">
+    <div className="homepage">
       <Searchbox />
+      <div className="homepage__heading">
+        Aktualna jakość powietrza w miejscowości{" "}
+        <span className="homepage__heading--bold">Zabierzów</span>
+      </div>
+      <div className="homepage__content">
+        <CardPollution />
+        <div className="homepage__content-image">
+          <img src={Town} alt="town view" />
+        </div>
+      </div>
     </div>
   );
 };
