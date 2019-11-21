cb9021008e1eace6ecff901d8ecdafda3180caeb
Ania Olak <anna.olak@lunarlogic.io>
2019-11-21 15:24:05 +0100

    Fix rubocop offenses

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,15 +1,14 @@
 FactoryBot.define do
-  
-    factory :user do
-      email { "joe@gmail.com" }
-      password { "blah456" }
-      confirmed_at { Time.current }
-    end
-
-    factory :admin, class: User do
-        email { "admin@gmail.com" }
-        password { "blah123" }
-        admin { true }
-        confirmed_at { Time.current }
-      end
+  factory :user do
+    email { 'joe@gmail.com' }
+    password { 'blah456' }
+    confirmed_at { Time.current }
   end
+
+  factory :admin, class: User do
+    email { 'admin@gmail.com' }
+    password { 'blah123' }
+    admin { true }
+    confirmed_at { Time.current }
+  end
+end
