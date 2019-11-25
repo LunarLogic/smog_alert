1ed1c17a6adb3446fbb768dc0f8ab0f45eca1780
Weronika Ciesielska <weronika.ciesielska@lunarlogic.io>
2019-11-25 10:59:40 +0100

    Move pollution comparison div from MapSection to separate component

--- app/javascript/packs/sections/Map/MapSection.js
+++ app/javascript/packs/sections/Map/MapSection.js
@@ -1,30 +1,9 @@
 import React from "react";
 import "./MapSection.scss";
-import mapSectionContent from "./mapSectionContent";
 import Map from "../../../../assets/images/Map.png";
-import PollutionBar from "../../components/PollutionBar/PollutionBar";
+import PollutionComparison from "../../components/PollutionComparison/PollutionComparison";
 
 const MapSection = () => {
-  const sortedPollutionData = mapSectionContent.sort(
-    (a, b) => b.value - a.value
-  );
-
-  const highestPollutionValue = sortedPollutionData[0].value;
-
-  const mapPollutionData = city => {
-    const { location, value, color } = city;
-    const width = (value * 100) / highestPollutionValue;
-    return (
-      <PollutionBar
-        key={location}
-        width={width}
-        backgroundColor={color}
-        location={location}
-        value={value}
-      />
-    );
-  };
-
   return (
     <div className="map-section">
       <div className="map-section__heading">
@@ -34,9 +13,7 @@ const MapSection = () => {
         <div className="map-section__content--map">
           <img src={Map} alt="map" />
         </div>
-        <div className="map-section__content--info">
-          {sortedPollutionData.map(mapPollutionData)}
-        </div>
+        <PollutionComparison />
       </div>
     </div>
   );
