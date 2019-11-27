10a1edf5972373e79cab3a6d031e42d56ac55129
Rozanna Bogacz <rozanna.bogacz@lunarlogic.io>
2019-11-27 16:46:28 +0100

    refactor airly API measurements test

--- spec/services/airly_api/measurements_spec.rb
+++ spec/services/airly_api/measurements_spec.rb
@@ -1,11 +1,12 @@
 describe AirlyAPI::Measurements do
+  let(:measurements) { described_class.new }
+
   describe '#point' do
+    subject { measurements.point('50.062006', '19.940984') }
+
     it do
       VCR.use_cassette "services/airly_api/measurements/point/get_measurements" do
-        measurements = described_class.new
-        response = measurements.point('50.062006', '19.940984')
-
-        expect(response['current']['values']).not_to be_empty
+        expect(subject['current']['values']).not_to be_empty
       end
     end
   end
