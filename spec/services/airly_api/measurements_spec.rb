db6e069dd3a5ceb31eb10adb8f7a1f1af964b263
Rozanna Bogacz <rozanna.bogacz@lunarlogic.io>
2019-11-27 15:45:42 +0100

    Record a cassette for point method

--- spec/services/airly_api/measurements_spec.rb
+++ spec/services/airly_api/measurements_spec.rb
@@ -1,9 +1,10 @@
 describe AirlyAPI::Measurements do
   describe '#point' do
     it do
-      measurements = described_class.new
-      response = measurements.point('50.062006', '19.940984')
-      byebug
+      VCR.use_cassette("services/airly_api/measurements/point/get_measurements") do
+        measurements = described_class.new
+        response = measurements.point('50.062006', '19.940984')
+      end
     end
   end
 end
