8c017c962b1e1865bf35980203d26652c4378c6f
Rozanna Bogacz <rozanna.bogacz@lunarlogic.io>
2019-11-27 16:20:21 +0100

    Expect airly API to return measurements

--- spec/services/airly_api/measurements_spec.rb
+++ spec/services/airly_api/measurements_spec.rb
@@ -1,9 +1,11 @@
 describe AirlyAPI::Measurements do
   describe '#point' do
     it do
-      VCR.use_cassette("services/airly_api/measurements/point/get_measurements") do
+      VCR.use_cassette "services/airly_api/measurements/point/get_measurements" do
         measurements = described_class.new
         response = measurements.point('50.062006', '19.940984')
+
+        expect(response['current']['values']).not_to be_empty
       end
     end
   end
