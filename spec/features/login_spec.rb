fb402367bcddf83b9bda1f19ce90715d2456c13b
madking55 <madking55@gmail.com>
2019-11-28 15:58:52 +0100

    Fix RuboCop offences

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -1,23 +1,23 @@
 require 'rails_helper'
 
-feature 'Login Page: Validate Page Elements' do
+feature 'Login page: Validate page elements' do
   background do
     visit 'http://localhost:3000/users/sign_in'
   end
 
-  scenario 'Displayed Title: Login' do
+  scenario 'Displayed title: Login' do
     expect(page).to have_css('h2', text: 'Log in')
   end
 
-  scenario 'Displayed Textbox: Email' do
+  scenario 'Displayed textbox: Email' do
     expect(page).to have_field('Email')
   end
 
-  scenario 'Displayed Textbox: Password' do
+  scenario 'Displayed textbox: Password' do
     expect(page).to have_field('Password')
   end
 
-  scenario 'Displayed Button: Log in' do
+  scenario 'Displayed button: Log in' do
     expect(page).to have_button('Log in')
   end
 end
