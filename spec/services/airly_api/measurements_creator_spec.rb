639fa1ff3a4a37ab5163314c2dc64878e80417cb
Ania Olak <anna.olak@lunarlogic.io>
2019-11-28 10:27:22 +0100

    Small refactor of MeasurementCreator test

--- spec/services/airly_api/measurements_creator_spec.rb
+++ spec/services/airly_api/measurements_creator_spec.rb
@@ -1,12 +1,12 @@
 describe AirlyAPI::MeasurementCreator do
-    #let(:measurement) { described_class.new(:location) }
+  subject { described_class.new(location) }
+
+  let(:location) { FactoryBot.create(:location) }
   
-    describe '#create' do
-      it do
-        location = FactoryBot.create(:location)
-        measurement = described_class.new(location)
-        measurement_object = measurement.create
-        byebug
-      end
+  describe '#create' do
+    it do
+      measurement_object = subject.create
+      byebug
     end
   end
+end
