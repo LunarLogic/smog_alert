6d138793720050f819e88db1a304a5a2fa87ab70
Ania Olak <anna.olak@lunarlogic.io>
2019-11-28 11:56:16 +0100

    Use data from airly request when creating measurement object

--- app/services/airly_api/measurement_creator.rb
+++ app/services/airly_api/measurement_creator.rb
@@ -5,17 +5,17 @@ module AirlyAPI
     end
 
     def create
-      measurements = Measurements.new.point(longitude, latitude)
+      airly_data = Measurements.new.point(longitude, latitude)
       @location.measurements.build(
-        date: Time.now.day,
-        hour: Time.now.hour,
-        pm10: 1.5,
-        pm25: 1.5,
-        temperature: 1.5,
-        humidity: 1.5,
-        pressure: 1.5,
-        from_date_time: Time.now,
-        till_date_time: Time.now
+        date: airly_data['current']['fromDateTime'][0,10],
+        hour: airly_data['current']['fromDateTime'][11,8], #TODO: fix cause there is a dummy date added because we use Time type in our database
+        pm10: airly_data['current']['values'].select { |e| e['name'] == 'PM10' }[0]['value'],
+        pm25: airly_data['current']['values'].select { |e| e['name'] == 'PM25' }[0]['value'],
+        temperature: airly_data['current']['values'].select { |e| e['name'] == 'temperature'.upcase }[0]['value'],
+        humidity: airly_data['current']['values'].select { |e| e['name'] == 'humidity'.upcase }[0]['value'],
+        pressure: airly_data['current']['values'].select { |e| e['name'] == 'pressure'.upcase }[0]['value'],
+        from_date_time: airly_data['current']['fromDateTime'],
+        till_date_time: airly_data['current']['tillDateTime']
       )
     end
 
