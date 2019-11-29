f1dd80478f01cf43df4e2819c1e7b39c987b93b6
Ania Olak <anna.olak@lunarlogic.io>
2019-11-29 15:08:57 +0100

    Fix rubocop offense by adding type in migration for removing hour column

--- db/migrate/20191128141238_change_measurements_hour_type.rb
+++ db/migrate/20191128141238_change_measurements_hour_type.rb
@@ -1,6 +1,6 @@
 class ChangeMeasurementsHourType < ActiveRecord::Migration[6.0]
   def change
-    remove_column :measurements, :hour
+    remove_column :measurements, :hour, :time
     add_column :measurements, :hour, :integer
 
     add_index :measurements, :hour
