43904723e894f6b2776091d8638f3e939c442808
Ania Olak <anna.olak@lunarlogic.io>
2019-11-29 19:16:46 +0100

    First draft of locations CRUD

--- app/controllers/admin/locations_controller.rb
+++ app/controllers/admin/locations_controller.rb
@@ -1,5 +1,35 @@
-class Admin::LocationsController < ApplicationController
+class Admin::LocationsController < Admin::BaseController
   def index
     @locations = Location.all
+  end
+
+  def show
+    @location = Location.find(params[:id])
+  end
+
+  def new
+    @location = Location.new
+  end
+
+  def create
+    @location = Location.new(location_params)
+    if @location.save
+      flash[:success] = 'Pomyślnie utworzono lokalizację'
+      redirect_to admin_location_path(@location)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def delete
+  end
+
+  private
+
+  def location_params
+    params.require(:location).permit(:name, :longitude, :latitude)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
 
   namespace :admin do
     root to: 'dashboard#index'
+    resources :locations
   end
 end
