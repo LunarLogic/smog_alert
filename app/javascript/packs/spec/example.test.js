0f2deeaed515405e8cf0a93f2a83b18145aa23c4
Weronika Ciesielska <weronika.ciesielska@lunarlogic.io>
2019-11-29 14:12:59 +0100

    Add Enzyme and CSS mocks

--- app/javascript/packs/spec/example.test.js
+++ app/javascript/packs/spec/example.test.js
@@ -1,7 +1,10 @@
-const add = (a, b) => {
-  return a + b;
-};
+import React from "react";
+import { mount } from "enzyme";
+import CustomButton from "../components/CustomButton/CustomButton";
 
-it("add", () => {
-  expect(add(1, 2)).toBe(3);
+describe("<CustomButton />", () => {
+  it("displays given text", () => {
+    const wrapper = mount(<CustomButton text="Button" />);
+    expect(wrapper.text()).toBe("Button");
+  });
 });
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  roots: ["<rootDir>/app/javascript/packs/spec"]
+  setupFiles: ["./app/javascript/packs/spec/support/enzyme"],
+  roots: ["<rootDir>/app/javascript/packs/spec"],
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/app/javascript/packs/spec/__mocks__/fileMock.js",
+    "\\.(css|scss)$":
+      "<rootDir>/app/javascript/packs/spec/__mocks__/styleMock.js"
+  }
 };
