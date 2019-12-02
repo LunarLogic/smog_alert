f7435805436e23cea93860a854bee60fa7f1f252
Ania Olak <anna.olak@lunarlogic.io>
2019-12-02 13:00:50 +0100

    Add validations to location model

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,6 @@
 class Location < ApplicationRecord
   has_many :measurements, dependent: :destroy
+  validates :name, presence: true, uniqueness: true
+  validates_presence_of :longitude
+  validates_presence_of :latitude
 end
