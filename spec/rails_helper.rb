af1236957e7a1038f583e7bf4ccb8aaf4f7ee435
Ania Olak <anna.olak@lunarlogic.io>
2019-12-02 16:13:16 +0100

    Fix rubocop offenses

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -65,8 +65,8 @@ RSpec.configure do |config|
   # https://github.com/plataformatec/devise/wiki/How-To:-sign-in-and-out-a-user-in-Request-type-specs-(specs-tagged-with-type:-:request)
   config.include Devise::Test::IntegrationHelpers, type: :request
 
-  Shoulda::Matchers.configure do |config|
-    config.integrate do |with|
+  Shoulda::Matchers.configure do |configuration|
+    configuration.integrate do |with|
       with.test_framework :rspec
       with.library :rails
     end
