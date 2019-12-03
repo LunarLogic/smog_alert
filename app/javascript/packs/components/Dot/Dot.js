a4d00b634415f722f70163404140336c43c7df5c
Olga Czarnecka <olga.czarnecka@lunarlogic.io>
2019-12-03 16:10:09 +0100

    Add onClick effects on map areas

--- app/javascript/packs/components/Dot/Dot.js
+++ app/javascript/packs/components/Dot/Dot.js
@@ -2,16 +2,20 @@ import React from "react";
 import "./Dot.scss";
 import { PropTypes } from "prop-types";
 
-const Dot = ({ className, backgroundColor }) => (
+const Dot = ({ id, className, backgroundColor, onClick }) => (
   <div
+    id={id}
     className={`dot ${className}`}
     style={{ backgroundColor: `${backgroundColor}` }}
+    onClick={onClick}
   />
 );
 
 Dot.propTypes = {
+  id: PropTypes.string,
   className: PropTypes.string,
-  backgroundColor: PropTypes.string
+  backgroundColor: PropTypes.string,
+  onClick: PropTypes.func
 };
 
 export default Dot;
--- app/javascript/packs/components/Map/Map.js
+++ app/javascript/packs/components/Map/Map.js
@@ -13,14 +13,45 @@ const Map = ({ citiesPollutionData }) => {
     getCitiesPollutionData(pollutionComparisonContent);
   });
 
+  let previouslyClickedElement;
+
+  const handleColorChange = event => {
+    let clickedElement = event.target;
+    let pathId;
+    switch (clickedElement.tagName) {
+      case "path":
+        pathId = clickedElement.id;
+        break;
+      case "DIV":
+        pathId = clickedElement.id.replace("dot--", "");
+        clickedElement = document.getElementById(pathId);
+        break;
+      case "text":
+        pathId = clickedElement.textContent;
+        clickedElement = document.getElementById(pathId);
+    }
+    if (previouslyClickedElement) {
+      previouslyClickedElement.removeAttribute("style");
+    }
+    citiesPollutionData.forEach(cityData => {
+      if (cityData.location === pathId) {
+        clickedElement.style.fill = cityData.color;
+        clickedElement.style.opacity = 0.5;
+        previouslyClickedElement = clickedElement;
+      }
+    });
+  };
+
   return (
     <div className="map">
-      <SVG src={MapImage} className="map__image" />
+      <SVG src={MapImage} className="map__image" onClick={handleColorChange} />
       {citiesPollutionData.map(cityData => (
         <Dot
           key={cityData.id}
+          id={`dot--${cityData.location}`}
           className={`dot--${cityData.location}`}
           backgroundColor={cityData.color}
+          onClick={handleColorChange}
         />
       ))}
     </div>
