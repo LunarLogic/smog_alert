823dbb6659b4c991d5e970d2121b798743c64cf5
Ania Olak <anna.olak@lunarlogic.io>
2019-12-03 14:30:04 +0100

    Fix airly API tests and record new VCR

--- spec/services/airly_api/measurements_creator_spec.rb
+++ spec/services/airly_api/measurements_creator_spec.rb
@@ -4,39 +4,39 @@ describe AirlyAPI::MeasurementCreator do
   let(:location) { FactoryBot.create(:location) }
 
   describe '#build' do
-    let(:measurement_object) { VCR.use_cassette('services/airly_api/measurements_zabierzow') { subject.build } }
+    let(:measurement_object) { VCR.use_cassette('services/airly_api/measurements_for_zabierzow') { subject.build } }
     it 'sets date correctly' do
-      expect(measurement_object.date).to eq(Date.parse('Thu, 28 Nov 2019'))
+      expect(measurement_object.date).to eq(Date.parse('Tue, 03 Dec 2019'))
     end
     it 'sets hour correctly' do
       expect(measurement_object.hour).to eq(13)
     end
     it 'sets PM10 correctly' do
-      expect(measurement_object.pm10).to eq(103.91)
+      expect(measurement_object.pm10).to eq(23.89)
     end
     it 'sets PM25 correctly' do
-      expect(measurement_object.pm25).to eq(60.15)
+      expect(measurement_object.pm25).to eq(12.61)
     end
     it 'sets temperature correctly' do
-      expect(measurement_object.temperature).to eq(7.73)
+      expect(measurement_object.temperature).to eq(-0.25)
     end
     it 'sets humidity correctly' do
-      expect(measurement_object.humidity).to eq(91.54)
+      expect(measurement_object.humidity).to eq(86.2)
     end
     it 'sets pressure correctly' do
-      expect(measurement_object.pressure).to eq(1000.02)
+      expect(measurement_object.pressure).to eq(1026.52)
     end
     it 'sets from_date_time correctly' do
-      expect(measurement_object.from_date_time).to eq(DateTime.parse('2019-11-28T12:56:41.684Z'))
+      expect(measurement_object.from_date_time).to eq(DateTime.parse('2019-12-03T12:17:30.582Z'))
     end
     it 'sets till_date_time correctly' do
-      expect(measurement_object.till_date_time).to eq(DateTime.parse('2019-11-28T13:56:41.684Z'))
+      expect(measurement_object.till_date_time).to eq(DateTime.parse('2019-12-03T13:17:30.582Z'))
     end
   end
 
   describe '#create' do
     it 'creates new measurement object' do
-      VCR.use_cassette 'services/airly_api/measurements_zabierzow' do
+      VCR.use_cassette 'services/airly_api/measurements_for_zabierzow' do
         expect { subject.create }.to change { Measurement.count }.by(1)
       end
     end
