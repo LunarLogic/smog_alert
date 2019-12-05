b75acbc806cc37c3db7b8389ee4c494a0e2c101f
Ania Olak <anna.olak@lunarlogic.io>
2019-12-05 18:31:57 +0100

    Fix rubocop offenses

--- spec/services/airly_extractor/measurement_data_spec.rb
+++ spec/services/airly_extractor/measurement_data_spec.rb
@@ -1,51 +1,49 @@
 describe AirlyExtractor::MeasurementData do
-
   describe '.extract' do
     it 'returns nil when there are no values' do
-      data = { 'current' => {'values' => []}}
+      data = {'current' => {'values' => []}}
       expect(described_class.extract(data)).to be nil
     end
     it 'sets value to nil when there is no data for a given measurement name' do
       data =
-        { 'current' =>
+        {
+          'current' =>
           {
-            'fromDateTime' => "2019-12-05T14:40:59.718Z",
-            'tillDateTime' => "2019-12-05T15:40:59.718Z",
+            'fromDateTime' => '2019-12-05T14:40:59.718Z',
+            'tillDateTime' => '2019-12-05T15:40:59.718Z',
             'values' =>
             [
               {
-                  "name" => "PM10",
-                  "value" => 74.01
+                'name' => 'PM10',
+                'value' => 74.01,
               },
               {
-                  "name" => "PRESSURE",
-                  "value" => 1022.66
+                'name' => 'PRESSURE',
+                'value' => 1022.66,
               },
               {
-                  "name" => "HUMIDITY",
-                  "value" => 76.24
+                'name' => 'HUMIDITY',
+                'value' => 76.24,
               },
               {
-                  "name" => "TEMPERATURE",
-                  "value"=> 1.08
-              }
-            ]
-          }
+                'name' => 'TEMPERATRE',
+                'value' => 1.08,
+              },
+            ],
+          },
         }
-        result = {
-          date: Date.new(2019, 12, 05),
-          hour: 15,
-          pm10: 74.01,
-          pm25: nil,
-          temperature: 1.08,
-          humidity: 76.24,
-          pressure: 1022.66,
-          from_date_time: "2019-12-05T14:40:59.718Z",
-          till_date_time: "2019-12-05T15:40:59.718Z",
-        }
-        expect(described_class.extract(data)).to eq(result)
-
+      result = {
+        date: Date.new(2019, 12, 5),
+        hour: 15,
+        pm10: 74.01,
+        pm25: nil,
+        temperature: 1.08,
+        humidity: 76.24,
+        pressure: 1022.66,
+        from_date_time: '2019-12-05T14:40:59.718Z',
+        till_date_time: '2019-12-05T15:40:59.718Z',
+      }
+      expect(described_class.extract(data)).to eq(result)
     end
   end
-
 end
