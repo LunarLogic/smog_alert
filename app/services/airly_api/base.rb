d6477a713a39ef69f251af47212947970678ebf6
Ania Olak <anna.olak@lunarlogic.io>
2019-12-05 15:29:48 +0100

    Small changes to airly api services

--- app/services/airly_api/base.rb
+++ app/services/airly_api/base.rb
@@ -1,11 +1,12 @@
 module AirlyAPI
   class Base
+    AIRLY_API_URL = 'https://airapi.airly.eu/v2/'
     private
 
     def headers
       {
         'Accept' => 'application/json',
-        'apikey' => Rails.application.credentials.dig(:airly_api_key),
+        'apikey' => Rails.application.credentials.airly_api_key,
       }
     end
   end
--- app/services/airly_api/measurements.rb
+++ app/services/airly_api/measurements.rb
@@ -1,7 +1,7 @@
 module AirlyAPI
   class Measurements < Base
     def point(lat, lng)
-      url = "https://airapi.airly.eu/v2/measurements/point?lat=#{lat}&lng=#{lng}"
+      url = "#{AIRLY_API_URL}measurements/point?lat=#{lat}&lng=#{lng}"
       response = RestClient.get(url, headers)
       JSON.parse(response.body)
     end
