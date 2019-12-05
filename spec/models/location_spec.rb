f7caef665e699ef97ff179ff6b38359c16745ff8
Ania Olak <anna.olak@lunarlogic.io>
2019-12-05 11:21:50 +0100

    Fix problem with cassette in measurements spec and remove unneeded line in location model test

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -2,9 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Location, type: :model do
   describe 'validations' do
-    # Here we're using FactoryBot, but you could use anything
-    subject { build(:location) }
-
     it { should validate_presence_of(:name) }
     it { should validate_uniqueness_of(:name) }
     it { should validate_presence_of(:longitude) }
