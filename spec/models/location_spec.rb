2040a6b3c5497c2f30afe521c445a019a0aac056
Ania Olak <anna.olak@lunarlogic.io>
2019-12-06 14:36:46 +0100

    Fix rubocop offenses

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Location, type: :model do
   describe 'validations' do
-    subject {FactoryBot.create(:location)}
+    subject { FactoryBot.create(:location) }
     it { should validate_uniqueness_of(:name) }
     it { should validate_presence_of(:longitude) }
     it { should validate_presence_of(:latitude) }
