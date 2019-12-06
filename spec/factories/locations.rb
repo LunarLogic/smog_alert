473672ea83295f168297f0988fc78c33ae2f6ed1
Ania Olak <anna.olak@lunarlogic.io>
2019-12-06 17:21:26 +0100

    First draft of locations spec

--- spec/factories/locations.rb
+++ spec/factories/locations.rb
@@ -4,4 +4,9 @@ FactoryBot.define do
     latitude { 50.116436 }
     longitude { 19.801319 }
   end
+  factory :fake_zabierzow, class: :location do
+    name { 'Zabierzow Fake' }
+    latitude { 50.116440 }
+    longitude { 19.801340 }
+  end
 end
--- spec/features/locations_spec.rb
+++ spec/features/locations_spec.rb
@@ -1,10 +1,32 @@
 require 'rails_helper'
 
-feature '#index' do
-  xit 'shows list of locations if user signed in' do
+describe 'admin interactions with locations' do
+  before :each do
     user = FactoryBot.create(:admin)
-    sign_in user
+    login_as(user, :scope => :user)
+  end
+
+  after :each do
+    Warden.test_reset!
+  end
+
+  # feature '#index' do
+  #   it 'shows table with locations' do
+  #     visit admin_locations_path
+  #     expect(page).to have_table('locations')
+  #   end
+  # end
+
+  # let(:new_location) { FactoryBot.create(:fake_zabierzow) }
+
+  scenario 'adding new location' do
     visit admin_locations_path
-    expect(page).to have_content(Location.first.name)
+    click_on('new-location')
+    expect(page).to have_content('Dodaj lokalizację')
+    fill_in 'Name', with: 'new_location.name'
+    fill_in 'Longitude', with: 'new_location.longitude'
+    fill_in 'Latitude', with: 'new_location.latitude'
+    click_on('Create Location')
+    expect(page).to have_content('new_location.name')
   end
 end
