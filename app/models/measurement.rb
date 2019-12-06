c43f92279a5b9967a4342a0ad9fd31b08f83c6bb
Ania Olak <anna.olak@lunarlogic.io>
2019-12-06 13:35:12 +0100

    Add calidations to measurement model

--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -1,3 +1,5 @@
 class Measurement < ApplicationRecord
   belongs_to :location
+  validates :from_date_time, presence: true
+  validates :till_date_time, presence: true
 end
--- spec/models/measurement_spec.rb
+++ spec/models/measurement_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Measurement, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:from_date_time) }
+  it { should validate_presence_of(:till_date_time) }
 end
