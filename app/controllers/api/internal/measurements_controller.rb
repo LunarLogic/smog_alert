d5ce4453b9abe6200235a94fa18231cb79cb1bde
Rozanna Bogacz <rozanna.bogacz@lunarlogic.io>
2019-12-12 16:05:11 +0100

    Fix rubocop offences

--- app/controllers/api/internal/measurements_controller.rb
+++ app/controllers/api/internal/measurements_controller.rb
@@ -11,9 +11,9 @@ class API::Internal::MeasurementsController < API::Internal::BaseController
 
     def to_hash
       if @last_hour_measurement
-        { location_id: @location.id, location_name: @location.name, lat: @location.latitude, lng: @location.longitude, last_hour_measurement: { from_date_time: @last_hour_measurement.from_date_time, till_date_time: @last_hour_measurement.till_date_time, values: { pm10: @last_hour_measurement.pm10, pm25: @last_hour_measurement.pm25} } }
-        else
-        { location_id: @location.id, location_name: @location.name, lat: @location.latitude, lng: @location.longitude, last_hour_measurement: { from_date_time: nil, till_date_time: nil, values: { pm10: nil, pm25: nil} } }
+        { location_id: @location.id, location_name: @location.name, lat: @location.latitude, lng: @location.longitude, last_hour_measurement: { from_date_time: @last_hour_measurement.from_date_time, till_date_time: @last_hour_measurement.till_date_time, values: { pm10: @last_hour_measurement.pm10, pm25: @last_hour_measurement.pm25 } } }
+      else
+        { location_id: @location.id, location_name: @location.name, lat: @location.latitude, lng: @location.longitude, last_hour_measurement: { from_date_time: nil, till_date_time: nil, values: { pm10: nil, pm25: nil } } }
       end
     end
   end
