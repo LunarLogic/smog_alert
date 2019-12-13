f60159959ca8ce593db0c2f8814e178455d9d40f
Rozanna Bogacz <rozanna.bogacz@lunarlogic.io>
2019-12-13 16:38:01 +0100

    Move LocationWithLastMeasurementPresenter to presenters catalogue

--- app/controllers/api/internal/measurements_controller.rb
+++ app/controllers/api/internal/measurements_controller.rb
@@ -1,40 +1,6 @@
 class API::Internal::MeasurementsController < API::Internal::BaseController
-  class LocationWithLastMeasurementPresenter
-    def self.collection(locations)
-      locations.map { |location| new(location) }
-    end
-
-    def initialize(location)
-      @location = location
-      @last_hour_measurement = location.last_hour_measurement
-    end
-
-    def to_hash
-      {
-        location_id: @location.id,
-        location_name: @location.name,
-        lat: @location.latitude,
-        lng: @location.longitude,
-        last_hour_measurement: last_hour_measurement_values,
-      }
-    end
-
-    def last_hour_measurement_values
-      return nil unless @last_hour_measurement
-
-      {
-        from_date_time: @last_hour_measurement.from_date_time,
-        till_date_time: @last_hour_measurement.till_date_time,
-        values: {
-          pm10: @last_hour_measurement.pm10,
-          pm25: @last_hour_measurement.pm25,
-        }
-      }
-    end
-  end
-
   def current
     locations = Location.all
-    render json: { data: LocationWithLastMeasurementPresenter.collection(locations) }
+    render json: { data: API::Internal::LocationWithLastMeasurementPresenter.collection(locations) }
   end
 end
---
+++ app/presenters/api/internal/location_with_last_measurement_presenter.rb
@@ -0,0 +1,33 @@
+class API::Internal::LocationWithLastMeasurementPresenter
+  def self.collection(locations)
+    locations.map { |location| new(location) }
+  end
+
+  def initialize(location)
+    @location = location
+    @last_hour_measurement = location.last_hour_measurement
+  end
+
+  def to_hash
+    {
+      location_id: @location.id,
+      location_name: @location.name,
+      lat: @location.latitude,
+      lng: @location.longitude,
+      last_hour_measurement: last_hour_measurement_values,
+    }
+  end
+
+  def last_hour_measurement_values
+    return nil unless @last_hour_measurement
+
+    {
+      from_date_time: @last_hour_measurement.from_date_time,
+      till_date_time: @last_hour_measurement.till_date_time,
+      values: {
+        pm10: @last_hour_measurement.pm10,
+        pm25: @last_hour_measurement.pm25,
+      }
+    }
+  end
+end
