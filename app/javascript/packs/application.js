87ab2a8f4a12c966d87fe261b6a1e5f77ddb7853
Olga Czarnecka <olga.czarnecka@lunarlogic.io>
2019-12-17 15:46:54 +0100

    Basic honeybadger setup

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,12 +3,23 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
+import Honeybadger from "honeybadger-js";
+import ErrorBoundary from "@honeybadger-io/react";
+
+const config = {
+  apiKey: "586828b9",
+  environment: window.location.hostname
+};
+
+const honeybadger = Honeybadger.configure(config);
 
 document.addEventListener("DOMContentLoaded", () => {
   ReactDOM.render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
+    <ErrorBoundary honeybadger={honeybadger}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ErrorBoundary>,
     document.body.appendChild(document.createElement("div"))
   );
 });
