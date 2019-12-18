f7f75e78ffd0698e19b69351b4a20a0dec7f9b7c
Olga Czarnecka <olga.czarnecka@lunarlogic.io>
2019-12-18 15:27:41 +0100

    Add NODE_ENV and disabled environments for honeybadger-js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,15 +3,8 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
-import Honeybadger from "honeybadger-js";
 import ErrorBoundary from "@honeybadger-io/react";
-
-const config = {
-  apiKey: "586828b9",
-  environment: window.location.hostname
-};
-
-const honeybadger = Honeybadger.configure(config);
+import { honeybadger } from "./honeybadger";
 
 document.addEventListener("DOMContentLoaded", () => {
   ReactDOM.render(
