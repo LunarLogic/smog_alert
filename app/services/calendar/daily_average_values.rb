f2c53b7544d8af37c97794e16e539064f26583d3
Ania Olak <anna.olak@lunarlogic.io>
2019-12-19 16:51:13 +0100

    Fix rubocop offenses

--- app/services/calendar/daily_average_values.rb
+++ app/services/calendar/daily_average_values.rb
@@ -4,12 +4,12 @@ module Calendar
     end
 
     def call(location, year)
-      year_measurements = location.measurements.where(date: Date.new(year,01,1)..Date.new(year,12,-1))
+      year_measurements = location.measurements.where(date: Date.new(year, 1, 1)..Date.new(year, 12, -1))
       days = year_measurements.distinct.pluck(:date)
       daily_measurements = []
       days.each do |day|
         day_measurements = year_measurements.where(date: day)
-        daily_measurements << {day: day, pm10: day_measurements.average(:pm10), pm25: day_measurements.average(:pm25)}
+        daily_measurements << { day: day, pm10: day_measurements.average(:pm10), pm25: day_measurements.average(:pm25) }
       end
       daily_measurements
     end
