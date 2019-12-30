a01bcece8c3e4ee50dbbb2e6f69ccfb90c306572
Ania Olak <anna.olak@lunarlogic.io>
2019-12-30 15:01:39 +0100

    add street column to locations database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_18_173512) do
+ActiveRecord::Schema.define(version: 2019_12_30_135931) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_12_18_173512) do
     t.float "latitude", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "street"
     t.index ["name"], name: "index_locations_on_name", unique: true
   end
 
