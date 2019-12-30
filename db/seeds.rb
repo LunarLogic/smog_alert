847c3f033899230a3bf5ff4028627375d7fa2396
Ania Olak <anna.olak@lunarlogic.io>
2019-12-30 12:30:33 +0100

    Fix rubocop offenses

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,43 +18,43 @@ cities = [
     id: 883,
     street: nil,
     number: nil,
-    sponsor: "Zabierzów"
+    sponsor: 'Zabierzów'
   },
   {
     name: 'Balice',
     latitude: 50.086516,
     longitude: 19.791139,
     id: 2274,
-    street: "Radziwiłłów",
-    number: "1",
-    sponsor: "Zabierzów"
+    street: 'Radziwiłłów',
+    number: '1',
+    sponsor: 'Zabierzów'
   },
   {
     name: 'Bolechowice',
     latitude: 50.13258,
     longitude: 19.784113,
     id: 503,
-    street: "Zielona",
-    number: "245",
-    sponsor: "Anonymous SmogFighter",
+    street: 'Zielona',
+    number: '245',
+    sponsor: 'Anonymous SmogFighter',
   },
   {
     name: 'Bolechowice',
     latitude: 50.147556,
     longitude: 19.793444,
     id: 413,
-    street: "Szkolna",
-    number: "37",
-    sponsor: "Zabierzów",
+    street: 'Szkolna',
+    number: '37',
+    sponsor: 'Zabierzów',
   },
   {
     name: 'Brzezie',
     latitude: 50.124599,
     longitude: 19.822081,
     id: 397,
-    street: "Narodowa",
-    number: "37",
-    sponsor: "Zabierzów"
+    street: 'Narodowa',
+    number: '37',
+    sponsor: 'Zabierzów',
   },
   {
     name: 'Brzezinka',
@@ -63,16 +63,16 @@ cities = [
     id: 7458,
     street: nil,
     number: nil,
-    sponsor: "Zabierzów"
+    sponsor: 'Zabierzów'
   },
   {
     name: 'Morawica',
     latitude: 50.096481,
     longitude: 19.718288,
     id: 6095,
-    street: "Brzoskwinia",
-    number: "186",
-    sponsor: "Zabierzów"
+    street: 'Brzoskwinia',
+    number: '186',
+    sponsor: 'Zabierzów'
   },
   {
     name: 'Burów',
@@ -81,7 +81,7 @@ cities = [
     id: 931,
     street: nil,
     number: nil,
-    sponsor: "Zabierzów"
+    sponsor: 'Zabierzów'
   },
   {
     name: 'Karniowice',
@@ -90,25 +90,25 @@ cities = [
     id: 514,
     street: nil,
     number: nil,
-    sponsor: "Zabierzów"
+    sponsor: 'Zabierzów'
   },
   {
     name: 'Kobylany',
     latitude: 50.14645,
     longitude: 19.763271,
     id: 9946,
-    street: "Jana Pawła II",
-    number: "61",
-    sponsor: "Zabierzów"
+    street: 'Jana Pawła II',
+    number: '61',
+    sponsor: 'Zabierzów'
   },
   {
     name: 'Kochanów',
     latitude: 50.113705,
     longitude: 19.747568,
     id: 6116,
-    street: "Droga Krajowa 79",
-    number: "28",
-    sponsor: "Zabierzów"
+    street: 'Droga Krajowa 79',
+    number: '28',
+    sponsor: 'Zabierzów',
   },
   {
     name: 'Kleszczów',
@@ -117,7 +117,7 @@ cities = [
     id: 7566,
     street: nil,
     number: nil,
-    sponsor: "Zabierzów"
+    sponsor: 'Zabierzów',
   },
   {
     name: 'Młynka',
@@ -126,7 +126,7 @@ cities = [
     id: 8154,
     street: nil,
     number: nil,
-    sponsor: "Zabierzów"
+    sponsor: 'Zabierzów'
   },
   {
     name: 'Niegoszowice',
@@ -135,25 +135,25 @@ cities = [
     id: 6649,
     street: nil,
     number: nil,
-    sponsor: "Zabierzów"
+    sponsor: 'Zabierzów'
   },
   {
     name: 'Nielepice',
     latitude: 50.110119,
     longitude: 19.700527,
     id: 5707,
-    street: "Długa",
+    street: 'Długa',
     number: nil,
-    sponsor: "Zabierzów",
+    sponsor: 'Zabierzów',
   },
   {
     name: 'Nielepice',
     latitude: 50.10684,
     longitude: 19.70766,
     id: 7413,
-    street: "Józefa Trzaskowskiego",
+    street: 'Józefa Trzaskowskiego',
     number: nil,
-    sponsor: "Anonymous SmogFighter",
+    sponsor: 'Anonymous SmogFighter',
   },
   {
     name: 'Pisary',
@@ -162,106 +162,106 @@ cities = [
     id: 10478,
     street: nil,
     number: nil,
-    sponsor: "Zabierzów",
+    sponsor: 'Zabierzów',
   },
   {
     name: 'Radwanowice',
     latitude: 50.148193,
     longitude: 19.709249,
     id: 6093,
-    street: "21 Lipca",
-    number: "56",
-    sponsor: "Zabierzów",
+    street: '21 Lipca',
+    number: '56',
+    sponsor: 'Zabierzów',
   },
   {
     name: 'Rząska',
     latitude: 50.099709,
     longitude: 19.837011,
     id: 493,
-    street: "Krakowska",
-    number: "122",
-    sponsor: "Zabierzów"
+    street: 'Krakowska',
+    number: '122',
+    sponsor: 'Zabierzów',
   },
   {
     name: 'Rudawa',
     latitude: 50.122887,
     longitude: 19.707764,
     id: 388,
-    street: "Polaczka",
-    number: "25",
-    sponsor: "Zabierzów"
+    street: 'Polaczka',
+    number: '25',
+    sponsor: 'Zabierzów',
   },
   {
     name: 'Szczyglice',
     latitude: 50.093582,
     longitude: 19.808323,
     id: 491,
-    street: "Sportowa",
-    number: "38",
-    sponsor: "Zabierzów",
+    street: 'Sportowa',
+    number: '38',
+    sponsor: 'Zabierzów',
   },
   {
     name: 'Ujazd',
     latitude: 50.137611,
     longitude: 19.818424,
     id: 482,
-    street: "Świerkowa",
-    number: "80",
-    sponsor: "Zabierzów",
+    street: 'Świerkowa',
+    number: '80',
+    sponsor: 'Zabierzów',
   },
   {
     name: 'Więckowice',
     latitude: 50.135612,
     longitude: 19.759342,
     id: 2655,
-    street: "Słoneczna",
-    number: "34",
-    sponsor: "Zabierzów"
+    street: 'Słoneczna',
+    number: '34',
+    sponsor: 'Zabierzów'
   },
   {
     name: 'Zabierzów',
     latitude: 50.120655,
     longitude: 19.78093,
     id: 521,
-    street: "Przy Torze",
-    number: "63",
-    sponsor: "Airly Ambassador"
+    street: 'Przy Torze',
+    number: '63',
+    sponsor: 'Airly Ambassador'
   },
   {
     name: 'Zabierzów',
     latitude: 50.116028,
     longitude: 19.800639,
     id: 9996,
-    street: "Kolejowa",
-    number: "26",
-    sponsor: "State Environmental Monitoring Station"
+    street: 'Kolejowa',
+    number: '26',
+    sponsor: 'State Environmental Monitoring Station'
   },
   {
     name: 'Zabierzów',
     latitude: 50.116436,
     longitude: 19.801319,
     id: 2123,
-    street: "Wapienna",
+    street: 'Wapienna',
     number: nil,
-    sponsor: "Zabierzów"
+    sponsor: 'Zabierzów'
   },
   {
     name: 'Zelków',
     latitude: 50.15202,
     longitude: 19.803083,
     id: 615,
-    street: "Krakowska",
-    number: "225",
-    sponsor: "Anonymous SmogFighter",
+    street: 'Krakowska',
+    number: '225',
+    sponsor: 'Anonymous SmogFighter',
   },
   {
     name: 'Zelków',
     latitude: 50.15869,
     longitude: 19.79455,
     id: 7912,
-    street: "Jana Pawła II",
-    number: "24",
-    sponsor: "Zabierzów",
+    street: 'Jana Pawła II',
+    number: '24',
+    sponsor: 'Zabierzów',
   },
 
 ]
