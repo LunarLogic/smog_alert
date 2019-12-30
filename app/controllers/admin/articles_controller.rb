033a417e143dc236ceaabe83fb26ea0f85acb211
madking55 <madking55@gmail.com>
2019-12-30 19:09:02 +0100

    Add views for articles

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -1,22 +1,50 @@
 class Admin::ArticlesController < Admin::BaseController
   def index
+    @articles = Article.all
   end
 
   def new
+    @article = Article.new
   end
 
   def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:success] = 'Pomyślnie dodano wpis'
+      redirect_to admin_articles_path
+    else
+      render 'new'
+    end
   end
 
   def show
+    @article = Article.find(params[:id])
   end
 
   def edit
+    @article = Article.find(params[:id])
   end
 
   def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:success] = 'Pomyslnie edytowano wpis'
+      redirect_to admin_articles_path
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:success] = 'Pomyślnie usunięto wpis'
+    redirect_to admin_articles_path
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
   end
 end
