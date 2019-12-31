003dbb6f86266a8b2bac8327a253a7d0001e48b3
Rozanna Bogacz <rozanna.bogacz@lunarlogic.io>
2019-12-31 15:01:06 +0100

    Add CRUD for users to the admin panel

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,41 @@
+class Admin::UsersController < Admin::BaseController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.save
+    redirect_to admin_users_path
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to admin_users_path
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :admin)
+  end
+end
