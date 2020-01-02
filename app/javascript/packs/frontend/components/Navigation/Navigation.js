256202fc4ba8ed9f82134605eb33a3cea1bfebdb
Olga Czarnecka <olga.czarnecka@lunarlogic.io>
2020-01-02 13:25:25 +0100

    add logo

--- app/javascript/packs/frontend/components/Navigation/Navigation.js
+++ app/javascript/packs/frontend/components/Navigation/Navigation.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import { CustomButton } from "../";
 import navigationContent from "./navigationContent";
+import Logo from "../../assets/logo.jpg";
 
 import "./Navigation.scss";
 
@@ -11,7 +12,9 @@ const Navigation = () => {
   return (
     <header className="navigation">
       <div className="navigation__brand">
-        <div className="navigation__brand-logo"></div>
+        <div className="navigation__brand-logo">
+          <img className="navigation__brand-logo-img" src={Logo} alt="logo" />
+        </div>
         <div className="navigation__brand-name">{brand}</div>
       </div>
       <div className="navigation__links">
