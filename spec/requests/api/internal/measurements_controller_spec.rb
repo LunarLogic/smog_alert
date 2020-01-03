5a02a904384dde4198154022adf406917e397d59
Ania Olak <anna.olak@lunarlogic.io>
2020-01-03 15:41:28 +0100

    Add basic test for calendar_status api endpoint

--- spec/requests/api/internal/measurements_controller_spec.rb
+++ spec/requests/api/internal/measurements_controller_spec.rb
@@ -1,27 +1,45 @@
 require 'rails_helper'
 
-
-
-describe '/api/internal/measurements/calendar_values' do
+describe '/api/internal/measurements' do
   let(:location) { FactoryBot.create(:location) }
 
-  it 'repsonds with json containing the right data' do
-    location.measurements.create(FactoryBot.attributes_for(:measurement))
-    location_id = location.id
-    year = location.measurements.last.date.year
-    get calendar_values_api_internal_measurements_path(location_id: location_id, year: year)
-    json = JSON.parse(response.body)
-    expected_response = {
-      "year" => 2019,
-      "daily_average_measurements" => [
-        {
-          "day" => "2019-11-27",
-          "pm10" => "1.5",
-          "pm25" => "1.5",
-          "number_of_measurements" => 1
+  let(:measurement) { location.measurements.create(FactoryBot.attributes_for(:measurement)) }
+
+  describe 'get /calendar_values' do
+    it 'repsonds with json containing the right data' do
+      expected_response = {
+        'year' => measurement.date.year,
+        'daily_average_measurements' => [
+          {
+            'day' => measurement.date.to_s,
+            'pm10' => measurement.pm10.to_s,
+            'pm25' => measurement.pm25.to_s,
+            'number_of_measurements' => 1
+          },
+        ]
+      }
+      location_id = location.id
+      year = location.measurements.last.date.year
+      get calendar_values_api_internal_measurements_path(location_id: location_id, year: year)
+      json = JSON.parse(response.body)
+      expect(json).to eq(expected_response)
+    end
+  end
+
+  describe '/calendar_status' do
+    it 'repsonds with json containing the right data' do
+      expected_response = {
+        '2019' => {
+          'zbyt mało danych' => [
+            measurement.date.to_s,
+          ]
         }
-      ]
-    }
-    expect(json).to eq(expected_response)
+      }
+      location_id = location.id
+      year = location.measurements.last.date.year
+      get calendar_status_api_internal_measurements_path(location_id: location_id, year: year)
+      json = JSON.parse(response.body)
+      expect(json).to eq(expected_response)
+    end
   end
 end
