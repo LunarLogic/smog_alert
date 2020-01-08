bcc1002c97a823c8e0f25327af169f305caecc3d
Artur Trzop <artur.trzop@lunarlogic.io>
2020-01-08 14:17:13 +0100

    Create homepage_spec.rb

---
+++ spec/features/homepage_spec.rb
@@ -0,0 +1,10 @@
+describe 'Homepage' do
+  before do
+    visit root_path
+  end
+
+  it 'renders react components' do
+    expect(page).to have_content 'Aktualna jakość powietrza w miejscowości Zabierzów'
+    expect(page).to have_content 'Jakość powietrza w gminie Zabierzów'
+  end
+end
