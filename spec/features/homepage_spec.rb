c41ba7b771eb843294ad1bd1fad72b3d63f91abe
Artur Trzop <artur.trzop@lunarlogic.io>
2020-01-08 14:47:35 +0100

    Load seeds in test to verify they work and homepage can be rendered with seed data

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -1,6 +1,14 @@
 describe 'Homepage', :xchrome do # change :xchrome tag to :chrome to see tests running in Chrome browser
   before do
+    Rails.application.load_seed
+
     visit root_path
+  end
+
+  it 'verify seeds were loaded' do
+    expect(User.count).to be > 0
+    expect(Location.count).to be > 0
+    expect(Measurement.count).to be > 0
   end
 
   it 'renders react components' do
