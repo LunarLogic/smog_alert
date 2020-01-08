2d5b7166df7e068ff08c6085b90ac0601834187c
Artur Trzop <artur.trzop@lunarlogic.io>
2020-01-08 14:28:37 +0100

    Remove unneeded RSpec.configure block

--- spec/support/config/capybara.rb
+++ spec/support/config/capybara.rb
@@ -1,6 +1,4 @@
 require 'capybara/rails'
 require 'capybara/rspec'
 
-RSpec.configure do |config|
-  Capybara.default_driver = :selenium_chrome_headless
-end
+Capybara.default_driver = :selenium_chrome_headless
