86962b7765965d4e2992aa926b4617254e9883ca
Artur Trzop <artur.trzop@lunarlogic.io>
2020-01-08 14:29:40 +0100

    Allow to run tests in chrome (so we can see tests in chrome browser) by using chrome tag on the test file

--- spec/support/config/capybara.rb
+++ spec/support/config/capybara.rb
@@ -2,3 +2,13 @@ require 'capybara/rails'
 require 'capybara/rspec'
 
 Capybara.default_driver = :selenium_chrome_headless
+
+RSpec.configure do |config|
+  config.before(:each) do |example|
+    Capybara.current_driver = :selenium_chrome if example.metadata[:chrome]
+  end
+
+  config.after(:each) do
+    Capybara.use_default_driver
+  end
+end
