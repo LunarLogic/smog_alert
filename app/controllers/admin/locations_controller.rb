f7e3701c9a005b7b115321089b98fbb5637db1e2
Ania Olak <anna.olak@lunarlogic.io>
2020-01-13 14:40:53 +0100

    Add possibility to edit street for location

--- app/controllers/admin/locations_controller.rb
+++ app/controllers/admin/locations_controller.rb
@@ -44,6 +44,6 @@ class Admin::LocationsController < Admin::BaseController
   private
 
   def location_params
-    params.require(:location).permit(:name, :longitude, :latitude)
+    params.require(:location).permit(:name, :street, :longitude, :latitude)
   end
 end
