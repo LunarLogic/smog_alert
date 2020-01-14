54eb6e67681418f007d19a10074002ee10f08210
madking55 <madking55@gmail.com>
2020-01-14 18:49:49 +0100

    Add test for make_published method

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -6,4 +6,13 @@ RSpec.describe Article, type: :model do
     it { should validate_presence_of(:title) }
     it { should validate_presence_of(:body) }
   end
+
+  describe '#make_published' do
+    it 'changes publish and published_at params' do
+      article = FactoryBot.create(:article, published: false, published_at: nil)
+      article.make_published
+      expect(article.published).to be true
+      expect(article.published_at).not_to be nil
+    end
+  end
 end
