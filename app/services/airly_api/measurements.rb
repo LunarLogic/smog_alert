94f15540684e40cfed8995b24e0d27f3f72ccde7
Ania Olak <anna.olak@lunarlogic.io>
2020-01-14 18:31:13 +0100

    Change method name

--- app/services/airly_api/measurements.rb
+++ app/services/airly_api/measurements.rb
@@ -2,17 +2,17 @@ module AirlyAPI
   class Measurements < Base
     def point(lat, lng)
       url = "#{AIRLY_API_URL}measurements/point?lat=#{lat}&lng=#{lng}"
-      airly_response_body(url)
+      get_json(url)
     end
 
     def by_installation_id(id)
       url = "#{AIRLY_API_URL}measurements/installation?installationId=#{id}"
-      airly_response_body(url)
+      get_json(url)
     end
 
     private
 
-    def airly_response_body(url)
+    def get_json(url)
       response = begin
                    RestClient.get(url, headers)
                  rescue RestClient::ExceptionWithResponse => e
