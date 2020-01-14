d743722b63da35fa2a8e29322a94f64867695307
Ania Olak <anna.olak@lunarlogic.io>
2020-01-14 17:06:11 +0100

    Add column with installation_id to locations db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_07_152137) do
+ActiveRecord::Schema.define(version: 2020_01_14_160359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_01_07_152137) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "street"
+    t.integer "installation_id"
     t.index ["name", "street"], name: "index_locations_on_name_and_street", unique: true
     t.index ["name"], name: "index_locations_on_name"
   end
