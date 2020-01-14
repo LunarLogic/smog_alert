b1b93b8a0235babfaf784b2c0cfff740759b98d4
Ania Olak <anna.olak@lunarlogic.io>
2020-01-14 15:44:11 +0100

    Add method for requests to measurements/installation airly api endpoint

--- app/services/airly_api/measurements.rb
+++ app/services/airly_api/measurements.rb
@@ -2,6 +2,17 @@ module AirlyAPI
   class Measurements < Base
     def point(lat, lng)
       url = "#{AIRLY_API_URL}measurements/point?lat=#{lat}&lng=#{lng}"
+      airly_response_body(url)
+    end
+
+    def by_installation_id(id)
+      url = "#{AIRLY_API_URL}measurements/installation?installationId=#{id}"
+      airly_response_body(url)
+    end
+
+    private
+
+    def airly_response_body(url)
       response = RestClient.get(url, headers)
       JSON.parse(response.body)
     end
