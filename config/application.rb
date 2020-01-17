ca528a1c8b0b417d14303b10fe6649bf4a061760
Ania Olak <anna.olak@lunarlogic.io>
2020-01-17 16:14:45 +0100

    Add configuration for remote forms

--- config/application.rb
+++ config/application.rb
@@ -17,5 +17,8 @@ module SmogAlert
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # Include the authenticity token in remote forms
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
