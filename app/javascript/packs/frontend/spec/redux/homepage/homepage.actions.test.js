00c27a175a5e17dd53f938e9f5f4a963c9d4dbeb
Weronika Ciesielska <weronika.ciesielska@lunarlogic.io>
2020-01-17 11:38:07 +0100

    Add tests for homepage action, add axios-mock-adapter to package.json

---
+++ app/javascript/packs/frontend/spec/redux/homepage/homepage.actions.test.js
@@ -0,0 +1,32 @@
+import { getCitiesPollutionData } from "../../../redux/homepage/homepage.actions";
+import thunkMiddleware from "redux-thunk";
+import MockAdapter from "axios-mock-adapter";
+import axios from "axios";
+
+import configureMockStore from "redux-mock-store";
+import homepageActionTypes from "../../../redux/homepage/homepage.types";
+import mockCitiesPollutionData from "../../__mocks__/citiesPollutionDataMock.json";
+
+const mockStore = configureMockStore([thunkMiddleware]);
+
+const mockAdapter = new MockAdapter(axios);
+
+describe("homepage actions", () => {
+  it("handles requesting homepage API", () => {
+    mockAdapter
+      .onGet("/api/internal/measurements/current")
+      .reply(200, mockCitiesPollutionData);
+
+    const expectedAction = [
+      {
+        type: homepageActionTypes.GET_CITIES_POLLUTION_DATA,
+        payload: mockCitiesPollutionData.data
+      }
+    ];
+    const store = mockStore({ payload: {} });
+    const action = store.getActions();
+    return store.dispatch(getCitiesPollutionData()).then(() => {
+      expect(action).toEqual(expectedAction);
+    });
+  });
+});
