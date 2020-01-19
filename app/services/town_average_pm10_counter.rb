a4a9d933f82268417e4d93a6d3fda2e3befd42d7
madking55 <madking55@gmail.com>
2020-01-19 16:43:18 +0100

    Add logic for counting town average pm10 pollution

---
+++ app/services/town_average_pm10_counter.rb
@@ -0,0 +1,28 @@
+class TownAveragePM10Counter
+  def initialize(data)
+    @data = data
+  end
+
+  def call
+    towns = []
+    pm10 = []
+    town_with_average_pm10 = {}
+
+    data.each do |location|
+      town = location[:location_name]
+      pm10_for_location = location[:last_hour_measurement][:values].first[:value]
+      towns << town
+      pm10 << pm10_for_location
+      locations_in_town_counter = 1
+      if town_with_average_pm10.keys.include?(town)
+
+        town_with_average_pm10[town] += pm10_for_location
+        locations_in_town_counter += 1
+        town_with_average_pm10[town] = town_with_average_pm10[town] / locations_in_town_counter
+      else
+        town_with_average_pm10[town] = pm10_for_location
+      end
+      p town_with_average_pm10
+    end
+  end
+end
