2f96581071c758d1dcf64640166a698cfb17f209
Rozanna Bogacz <rozanna.bogacz@lunarlogic.io>
2020-01-24 13:15:42 +0100

    Remove index page

--- app/controllers/admin/organizations_controller.rb
+++ app/controllers/admin/organizations_controller.rb
@@ -1,8 +1,4 @@
 class Admin::OrganizationsController < Admin::BaseController
-  def index
-    @organizations = Organization.all
-  end
-
   def show
     @organization = Organization.find(params[:id])
   end
