f34d49315c54127283317f2fb6b9ed974fd62bd8
Ania Olak <anna.olak@lunarlogic.io>
2020-01-27 14:19:52 +0100

    Check if params for installations search are blank in controller instead of service

--- app/services/airly_api/installations.rb
+++ app/services/airly_api/installations.rb
@@ -1,8 +1,6 @@
 module AirlyAPI
   class Installations < Base
     def nearest(latitude, longitude, max_distance_km = nil, max_results = 5)
-      max_distance_km = nil if max_distance_km.blank?
-      max_results = 5 if max_results.blank?
       url = "#{AIRLY_API_URL}installations/nearest?lat=#{latitude}&lng=#{longitude}&maxResults=#{max_results}"
       url += "&maxDistanceKM=#{max_distance_km}" if max_distance_km
       get_json(url)
