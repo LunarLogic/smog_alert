f6480a45b24942e3922f1a3ad87f5a59ca6de013
Ania Olak <anna.olak@lunarlogic.io>
2020-01-27 15:42:29 +0100

    Remove to_params method from installation_search_form

--- app/forms/installation_search_form.rb
+++ app/forms/installation_search_form.rb
@@ -1,22 +1,10 @@
 class InstallationSearchForm
   include ActiveModel::Model
 
-  # validates :longitude, :latitude, presence: true
-  # validates :latitude, presence: true
-
   attr_accessor(
     :longitude,
     :latitude,
     :max_distance_km,
     :max_results,
   )
-
-  def to_params
-    {
-      longitude: longitude,
-      latitude: latitude,
-      max_distance_km: max_distance_km,
-      max_results: max_results,
-    }
-  end
 end
