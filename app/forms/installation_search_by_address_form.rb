92359d198f278cfe3925fac4250ec54951a0bc09
Ania Olak <anna.olak@lunarlogic.io>
2020-01-29 12:53:32 +0100

    Delete unnecessary method and move installation search by address form object

--- app/forms/installation_search_by_address_form.rb
+++ app/forms/installation_search_by_address_form.rb
@@ -6,12 +6,4 @@ class InstallationSearchByAddressForm
     :max_distance_km,
     :max_results,
   )
-
-  def to_params
-    {
-      address: address,
-      max_distance_km: max_distance_km,
-      max_results: max_results,
-    }
-  end
 end
