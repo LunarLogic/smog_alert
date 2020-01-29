e4c241abc831a26e6c5f41eb4e60627246fa0555
Weronika Ciesielska <weronika.ciesielska@lunarlogic.io>
2020-01-29 16:00:46 +0100

    Refactor rspec test for homepage

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -12,7 +12,8 @@ describe 'Homepage', :xchrome do # change :xchrome tag to :chrome to see tests r
   end
 
   it 'renders react components' do
-    expect(page).to have_content 'Aktualna jakość powietrza w miejscowości Zabierzów'
+    expect(page).to have_content 'Aktualna jakość powietrza w miejscowości'
+    expect(page).to have_content 'Zabierzów, Kolejowa 26'
     expect(page).to have_content 'Jakość powietrza w gminie Zabierzów'
   end
 end
