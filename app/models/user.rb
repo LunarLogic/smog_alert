9d7bea89009c74936074bd3c1e951aa3af9c46f3
madking55 <madking55@gmail.com>
2020-01-30 17:14:53 +0100

    Fix rubocop offences

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
 
   def self.from_omniauth(access_token)
     data = access_token.info
-    user = User.where(email: data['email']).first
+    user = User.find_by(email: data['email'])
 
     unless user
       password = Devise.friendly_token[0, 20]
