1317ca86589b40415c92a3c56cf084a76005b099
madking55 <madking55@gmail.com>
2020-02-04 13:13:42 +0100

    Fix rubocop offences

--- spec/features/login_with_google_spec.rb
+++ spec/features/login_with_google_spec.rb
@@ -7,26 +7,26 @@ RSpec.feature 'user logs in' do
     expect(page).to have_link('Sign in with Google')
     click_on('Sign in with Google')
     expect(page).to have_content('Continue to Smog Alert')
-  #  expect(page).to have_current_path(user_google_oauth2_omniauth_authorize_path)
+    # expect(page).to have_current_path(user_google_oauth2_omniauth_authorize_path)
     # expect(page).to have_content('Anna Nowak')
     # expect(page).to have_link('Logout')
   end
 
   def stub_omniauth
     OmniAuth.config.test_mode = true
-    OmniAuth.config.mock_auth[:google] = OmniAuth::AuthHash.new({
-      provider: "google",
-      uid: "12345678910",
+    OmniAuth.config.mock_auth[:google] = OmniAuth::AuthHash.new(
+      provider: 'google',
+      uid: '12345678910',
       info: {
         email: 'annanowak@example.com',
         first_name: 'Anna',
         last_name: 'Nowak'
       },
       credentials: {
-        token: "abcdefg12345",
-        refresh_token: "12345abcdefg",
+        token: 'abcdefg12345',
+        refresh_token: '12345abcdefg',
         expires_at: DateTime.now,
-      }
-    })
+      },
+    )
   end
 end
