cfe2ba1d0f3f7bb7ec3da26e3ed4b654fb41cc32
Rozanna Bogacz <rozanna.bogacz@lunarlogic.io>
2020-02-04 16:38:37 +0100

    Change access scope

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, Rails.application.credentials.google_client_id, Rails.application.credentials.google_client_secret, {
     name: 'google',
-    scope: ['contacts','plus.login','plus.me','email','profile'],
+    scope: ['email'],
     prompt: 'select_account',
     image_aspect_ratio: 'square',
     image_size: 50,
