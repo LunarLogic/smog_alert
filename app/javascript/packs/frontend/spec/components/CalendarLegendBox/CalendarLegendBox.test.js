b26f2d1c17a7d8f0aa1c786b1db9c63e02a1162f
Olga Czarnecka <olga.czarnecka@lunarlogic.io>
2020-02-05 15:42:08 +0100

    Add test for CalendarLegendBox

---
+++ app/javascript/packs/frontend/spec/components/CalendarLegendBox/CalendarLegendBox.test.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { mount } from "enzyme";
+import CalendarLegendBox from "../../../components/CalendarLegendBox/CalendarLegendBox";
+
+describe("<CalendarLegendBox/>", () => {
+  it("renders component with correct", () => {
+    const wrapperWithParameters = mount(
+      <CalendarLegendBox
+        backgroundColor={"blue"}
+        status={"bardzo dobry"}
+        numberOfDays={53}
+      />
+    );
+    expect(
+      wrapperWithParameters.find("StyledComponent").prop("backgroundColor")
+    ).toEqual("blue");
+    expect(wrapperWithParameters.find("span").text()).toEqual(
+      "bardzo dobry:53 dni"
+    );
+  });
+});
