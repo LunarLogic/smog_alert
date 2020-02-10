9e9abc15641c56c73fa65c1d3a21cd16f67c2f61
Weronika Ciesielska <weronika.ciesielska@lunarlogic.io>
2020-02-10 19:17:21 +0100

    Add default timezone for tests to package.json

--- app/javascript/packs/frontend/spec/helpers/getDate.test.js
+++ app/javascript/packs/frontend/spec/helpers/getDate.test.js
@@ -3,7 +3,7 @@ import { getDate } from "../../helpers";
 describe("getDate", () => {
   it("returns correct value when date string is provided", () => {
     const dateString = "2020-02-07T11:51:23.698Z";
-    expect(getDate(dateString)).toEqual("7 lutego 2020r., 12:51");
+    expect(getDate(dateString)).toEqual("7 lutego 2020r., 11:51");
   });
   it("returns correct data when date string is not provided", () => {
     expect(getDate()).toEqual("--");
