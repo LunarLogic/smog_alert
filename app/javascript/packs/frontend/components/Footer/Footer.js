cd041c926f62d1a8708f25389d61dc63123a60e5
Weronika Ciesielska <weronika.ciesielska@lunarlogic.io>
2020-02-11 17:44:17 +0100

    Refactor Footer component to handle displaying organization details received from API

--- app/javascript/packs/frontend/components/Footer/Footer.js
+++ app/javascript/packs/frontend/components/Footer/Footer.js
@@ -1,21 +1,26 @@
 import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import { PropTypes } from "prop-types";
 
 import { ContactDetail } from "../";
-import footerContent from "./footerContent";
 
+import { FooterLogo } from "./Footer.styles.jsx";
 import "./Footer.scss";
 
-const Footer = () => {
+import { selectOrganizationDetails } from "../../redux/application/application.selectors";
+
+const Footer = ({ organizationDetails }) => {
+  const { logo, name, description, email, facebook } = organizationDetails;
+  const footerContent = [email, facebook];
+
   return (
     <div className="footer">
       <div className="footer-container">
         <div className="footer__info">
-          <div className="footer__info-logo"></div>
-          <div className="footer__info-brand">Zabierzowski Alarm Smogowy</div>
-          <div className="footer__info-description">
-            Stowarzyszenie tworzone przez ludzi, którzy lubią czyste powietrze i
-            zielone drzewa
-          </div>
+          <FooterLogo image={logo} />
+          <div className="footer__info-brand">{name}</div>
+          <div className="footer__info-description">{description}</div>
           <div className="footer__info-credentials">
             W serwisie wykorzystano dane z pyłomierza własnego oraz dane z
             pyłomierzy firmy Airly, a także informacje o sile wiatru ze stacji
@@ -35,4 +40,12 @@ const Footer = () => {
   );
 };
 
-export default Footer;
+const mapStateToProps = createStructuredSelector({
+  organizationDetails: selectOrganizationDetails
+});
+
+Footer.propTypes = {
+  organizationDetails: PropTypes.object
+};
+
+export default connect(mapStateToProps)(Footer);
