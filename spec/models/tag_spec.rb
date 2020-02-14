53fafbd0dd2ab25d179950bc0b31cda5b5e2cce6
Ania Olak <anna.olak@lunarlogic.io>
2020-02-14 12:43:53 +0100

    Small improvements

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
   describe 'validations' do
-    subject { FactoryBot.create(:tag) }
     it { should validate_presence_of(:name) }
   end
 end
