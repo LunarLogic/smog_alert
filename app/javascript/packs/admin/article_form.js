18a8366ac630ef19a3ec644337b2e8afd2939fb1
Ania Olak <anna.olak@lunarlogic.io>
2020-02-14 12:43:42 +0100

    Improve jQuery for removing tag field using .on instead of .clinck

--- app/javascript/packs/admin/article_form.js
+++ app/javascript/packs/admin/article_form.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  function removeField(e) {
-    $('.btn-tag-field-remove').click(function(e) {
+  function attachRemoveHandlers(e) {
+    $('#js-tags-fields').on('click', '.tag-field button.close', function(e) {
       e.preventDefault();
       $(this).parent('.tag-field:first').remove();
     });
@@ -8,15 +8,14 @@ $(document).ready(function() {
 
   $('#js-add-tag-field-button').click(function(e) {
     e.preventDefault();
-    var index = $('#js-new-tags-fields').children().length + 1
+    var index = $('#js-tags-fields').children().length + 1
     inputTemplate = `<div class="tag-field d-flex">
       <div class="form-group string required article_tags_name">
       <input class="form-control string required" placeholder="Dodaj tag" type="text" name="article[tags_attributes][${index}][name]">
       </div>
       <button type='button' class='close ml-2 btn-tag-field-remove' aria-label='Close'><span aria-hidden='true'>&times;</span></button>
       </div>`;
-    $('#js-new-tags-fields').append(inputTemplate);
-    removeField();
+    $('#js-tags-fields').append(inputTemplate);
   });
-  removeField();
+  attachRemoveHandlers();
 })
