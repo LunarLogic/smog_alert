d091bc0a54f2fd5c8b4a14e67a6242b5addc6d5f
Anna Ślimak <slimakania@gmail.com>
2020-02-18 13:35:20 +0100

    wip

--- app/javascript/packs/admin/article_form.js
+++ app/javascript/packs/admin/article_form.js
@@ -1,21 +1,17 @@
 $(document).ready(function() {
   function attachRemoveHandlers(e) {
-    $('#js-tags-fields').on('click', '.tag-field button.close', function(e) {
+    $("#js-tags-fields").on("click", ".tag-field button.close", function(e) {
       e.preventDefault();
-      $(this).parent('.tag-field:first').remove();
+      $(this)
+        .parent(".tag-field:first")
+        .remove();
     });
-  };
+  }
 
-  $('#js-add-tag-field-button').click(function(e) {
+  $("#js-add-tag-field-button").click(function(e) {
     e.preventDefault();
-    var index = $('#js-tags-fields').children().length + 1
-    inputTemplate = `<div class="tag-field d-flex">
-      <div class="form-group string required article_tags_name">
-      <input class="form-control string required" placeholder="Dodaj tag" type="text" name="article[tags_attributes][${index}][name]">
-      </div>
-      <button type='button' class='close ml-2 btn-tag-field-remove' aria-label='Close'><span aria-hidden='true'>&times;</span></button>
-      </div>`;
-    $('#js-tags-fields').append(inputTemplate);
+    const newTag = $("#js-tag-template").html();
+    $("#js-tags-fields").append(newTag);
   });
   attachRemoveHandlers();
-})
+});
