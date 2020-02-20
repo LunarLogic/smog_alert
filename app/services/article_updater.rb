37c3c59ac61210d1d1b33a19446177c3fee37773
Ania Olak <anna.olak@lunarlogic.io>
2020-02-20 14:42:25 +0100

    Write method for updating article's title and body

--- app/services/article_updater.rb
+++ app/services/article_updater.rb
@@ -5,17 +5,25 @@ class ArticleUpdater
 
   def call(params)
     manage_tags(params[:tags_attributes])
+    save_article(params[:title], params[:body])
   end
 
   def manage_tags(tags_attributes)
-    tags_to_stay_ids = tags_attributes.present? ? tags_attributes.map { |tag| tag[:id] } : nil
-    delete_tags(tags_to_stay_ids)
+    edit_tags(tags_attributes)
+    delete_tags(tags_attributes)
   end
 
   def save_article(title, body)
+    @article.title = title
+    @article.body = body
   end
 
-  def delete_tags(ids)
+  def edit_tags(tags_attributes)
+
+  end
+
+  def delete_tags(tags_attributes)
+    ids = tags_attributes.present? ? tags_attributes.map { |tag| tag[:id] } : nil
     Tagging.where(article_id: @article.id).where.not(tag_id: ids).delete_all
     Tag.left_outer_joins(:taggings).where('tag_id is null').delete_all
   end
