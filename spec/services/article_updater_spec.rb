c7605c20c4f66cca48a26a821399b1d5d214da7e
Ania Olak <anna.olak@lunarlogic.io>
2020-02-20 15:49:20 +0100

    Add test for editing tags

--- spec/services/article_updater_spec.rb
+++ spec/services/article_updater_spec.rb
@@ -28,6 +28,19 @@ RSpec.describe ArticleUpdater do
     expect(Tag.all.count).to eq(0)
   end
 
+  context 'manage editing of tags' do
+    it 'deletes relation with a tag if it\'s name was changed' do
+      tag1 = article.tags.create(name: 'tag1')
+      article.tags.create(name: 'tag2')
+      tag3 = Tag.create(name: 'tag3')
+      params[:tags_attributes] = [{ name: tag3.name, id: tag1.id }]
+      ArticleUpdater.new(article).call(params)
+      expect(article.tags.last.name).to eq(tag3.name)
+      expect(article.tags.last.id).to eq(tag3.id)
+      expect(article.tags.all).to_not include(tag1)
+    end
+  end
+
   it 'updates article\'s title and body' do
     params[:title] = 'New title'
     params[:body] = 'New body'
