9dd51c60d1c4ab0030abe0a72be499fe234e1dd6
Ania Olak <anna.olak@lunarlogic.io>
2020-02-22 10:26:09 +0100

    Validate unigueness of tag name

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,6 @@
 class Tag < ApplicationRecord
   validates :name, presence: true
+  validates :name, uniqueness: true
 
   has_many :taggings, dependent: :destroy
   has_many :articles, through: :taggings
