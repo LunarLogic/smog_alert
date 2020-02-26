7bb5530f98845c6c284e2d3ca2428e03718f7122
Artur Trzop <artur.trzop@lunarlogic.io>
2020-02-26 12:26:34 +0100

    Add api docs for get single article

--- app/controllers/api/internal/articles_controller.rb
+++ app/controllers/api/internal/articles_controller.rb
@@ -3,9 +3,7 @@ class API::Internal::ArticlesController < API::Internal::BaseController
 
   swagger_path '/api/internal/articles' do
     operation :get do
-      key :summary, 'All published articles'
-      key :description, 'Returns all published articles by admin'
-      # key :operationId, 'findPets'
+      key :summary, 'Get all published articles'
       key :produces, [
         'application/json',
       ]
@@ -37,6 +35,65 @@ class API::Internal::ArticlesController < API::Internal::BaseController
               property :updated_at do
                 key :type, :string
               end
+            end
+          end
+        end
+      end
+    end
+  end
+
+  swagger_path '/api/internal/articles/{id}' do
+    operation :get do
+      key :summary, 'Get published article by ID'
+      key :produces, [
+        'application/json',
+      ]
+      key :tags, [
+        'articles',
+      ]
+      parameter do
+        key :name, :id
+        key :description, 'Article ID'
+        key :in, :path
+        key :required, true
+        key :type, :integer
+      end
+      response 200 do
+        key :description, 'Article'
+        schema do
+          key :type, :object
+          property :data do
+            key :type, :object
+            property :id do
+              key :type, :integer
+            end
+            property :title do
+              key :type, :string
+            end
+            property :body do
+              key :type, :string
+            end
+            property :published_at do
+              key :type, :string
+            end
+            property :updated_at do
+              key :type, :string
+            end
+          end
+        end
+      end
+      response 404 do
+        key :description, 'Article not found'
+        schema do
+          key :type, :object
+          property :data do
+            key :type, :string
+            key :description, 'Default value is null'
+          end
+          property :errors do
+            key :type, :array
+            items do
+              key :type, :string
             end
           end
         end
