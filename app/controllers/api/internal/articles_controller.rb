f6eb1cd728459ededbe693d6d6870e1fb786e862
madking55 <madking55@gmail.com>
2020-02-26 17:42:02 +0100

    Add pagination methods

--- app/controllers/api/internal/articles_controller.rb
+++ app/controllers/api/internal/articles_controller.rb
@@ -1,10 +1,20 @@
 class API::Internal::ArticlesController < API::Internal::BaseController
   def index
-    data = ArticlesRepository.new.published_articles.map do |article|
-      API::Internal::ArticleOverviewPresenter.new(article)
-    end
+    # data = ArticlesRepository.new.published_articles.map do |article|
+    #   API::Internal::ArticleOverviewPresenter.new(article)
+    # end
+    data = Article.page(params[:page]).per(params[:per_page])
 
-    render json: { data: data }
+    render json:
+    {
+      data: data,
+      meta: { pagination:
+              {
+                per_page: params[:per_page],
+                total_pages: data.total_pages,
+                total_objects: data.total_count
+              } }
+    }
   end
 
   def show
