2ee1ddccc89565e8dbcd7e2905c3e4f69f9c52c2
Ania Olak <anna.olak@lunarlogic.io>
2020-02-26 16:44:08 +0100

    Remove unncessary things

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,9 +6,6 @@ class Article < ApplicationRecord
 
   paginates_per 5
 
-  accepts_nested_attributes_for :tags, allow_destroy: true, reject_if: proc { |attributes| attributes[:name].blank? }
-
   validates :title, :body, :overview, :user_id, presence: true
   validates :overview, length: { maximum: 1500 }
-  # validates_associated :tags
 end
