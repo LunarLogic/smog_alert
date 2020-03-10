a5d4703bf4302401c6dcce4e24193586541f8d66
Anna Ślimak <slimakania@gmail.com>
2020-03-10 10:59:21 +0100

    Fixed tests

--- app/javascript/packs/application-admin.js
+++ app/javascript/packs/application-admin.js
@@ -3,7 +3,7 @@ require("@rails/actiontext");
 require("jquery");
 require("jquery_ui");
 require("@rails/ujs").start();
-require("turbolinks").start();
+// require("turbolinks").start();
 // require("@rails/activestorage").start();
 // require("channels");
 
