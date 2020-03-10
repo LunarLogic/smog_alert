442d124a1c336220e718dc80b4433169eaf9f4e5
Ania Olak <anna.olak@lunarlogic.io>
2020-03-10 13:47:03 +0100

    Delete obsolete tags after deleting the article

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -41,6 +41,7 @@ class Admin::ArticlesController < Admin::BaseController
 
   def destroy
     @article.destroy
+    article_tags_repository.delete_obsolete_tags
     flash[:success] = 'Pomyślnie usunięto wpis'
     redirect_to admin_articles_path
   end
@@ -75,6 +76,10 @@ class Admin::ArticlesController < Admin::BaseController
 
   def articles_repository
     ArticlesRepository.new
+  end
+
+  def article_tags_repository
+    ArticleTagsRepository.new
   end
 
   def article_params
