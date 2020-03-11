3c42567d357dde3f8138e16d88c6119da7228b3b
Ania Olak <anna.olak@lunarlogic.io>
2020-03-11 17:53:25 +0100

    Change the way to get measurements from last hour for each installation

--- app/repositories/locations_repository.rb
+++ app/repositories/locations_repository.rb
@@ -2,4 +2,8 @@ class LocationsRepository
   def ids_of_installations_in_db(ids)
     Location.pluck(:installation_id) & ids
   end
+
+  def locations_with_measurements_from_last_n_hours(n)
+    Location.eager_load(:measurements).where(['measurements.till_date_time >= ?', (Time.current - n.hour)])
+  end
 end
