a6f3bd3aa37814bfed53ec7b31067bbaf1e2b93b
Ania Olak <anna.olak@lunarlogic.io>
2020-03-13 18:44:15 +0100

    Add test for location with last measuremetn presenter

--- spec/services/last_hour_measurement_assigner_spec.rb
+++ spec/services/last_hour_measurement_assigner_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe LastHourMeasurementsAssigner do
   let(:location) { FactoryBot.create(:location) }
+  let(:location_2) { FactoryBot.create(:location) }
+
   it 'it returns array with hash with last hour measurement' do
     measurement = FactoryBot.create(:measurement, till_date_time: Time.current, location: location)
     expected_response = [
@@ -25,23 +27,17 @@ RSpec.describe LastHourMeasurementsAssigner do
           advice: measurement.advice
         },
       },
-    ]
-    expect(subject.call([location])).to eq(expected_response)
-  end
-
-  it 'it returns array with hash with nil as last hour measurement' do
-    expected_response = [
       {
-        location_id: location.id,
-        location_name: location.name,
-        location_street: location.street,
-        location_display_name: location.name,
-        lat: location.latitude,
-        lng: location.longitude,
+        location_id: location_2.id,
+        location_name: location_2.name,
+        location_street: location_2.street,
+        location_display_name: location_2.name,
+        lat: location_2.latitude,
+        lng: location_2.longitude,
         status_of_locations_grouped_by_name: nil,
-        last_hour_measurement: nil,
+        last_hour_measurement: nil
       },
     ]
-    expect(subject.call([location])).to eq(expected_response)
+    expect(subject.call([location, location_2])).to eq(expected_response)
   end
 end
