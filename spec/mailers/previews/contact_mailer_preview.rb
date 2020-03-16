d02d979d201e8c991463a9e339f5af1821cc9701
madking55 <madking55@gmail.com>
2020-03-16 20:47:32 +0100

    Fix rubocop offences, remove unnecessary files

--- spec/mailers/previews/contact_mailer_preview.rb
+++ spec/mailers/previews/contact_mailer_preview.rb
@@ -2,11 +2,11 @@
 class ContactMailerPreview < ActionMailer::Preview
   def contact_form_email
     contact_form_params = {
-      sender_name: "Jan Kowalski",
-      message: "Hello from the Smog Alert contact form!",
-      sender_email: "jan.kowalski@example.com"
+      sender_name: 'Jan Kowalski',
+      message: 'Hello from the Smog Alert contact form!',
+      sender_email: 'jan.kowalski@example.com'
     }
-    
+
     ContactMailer.with(contact_form_params).contact_form_email
   end
 end
