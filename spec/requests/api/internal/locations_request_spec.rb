4c18b5fab617a8407e1454417584935ef5bcd748
Ania Olak <anna.olak@lunarlogic.io>
2020-03-18 16:21:51 +0100

    Fix failing test

--- spec/requests/api/internal/locations_request_spec.rb
+++ spec/requests/api/internal/locations_request_spec.rb
@@ -3,13 +3,15 @@ require 'rails_helper'
 describe '/api/internal/locations' do
   describe 'GET /no_current_measurements' do
     it 'repsonds with json containing locations that don\'t have measurement from last hour' do
-      location1 = FactoryBot.create(:location)
-      location2 = FactoryBot.create(:location)
-      location3 = FactoryBot.create(:location)
-      FactoryBot.create(:measurement, location_id: location3.id, till_date_time: 1.hour.ago)
-      expected_response = [location1, location2].to_json
-      get no_current_measurements_api_internal_locations_path
-      expect(response.body).to be_json_eql(expected_response)
+      freeze_time do
+        location1 = FactoryBot.create(:location, created_at: 5.minutes.ago)
+        location2 = FactoryBot.create(:location, created_at: 2.minutes.ago)
+        location3 = FactoryBot.create(:location)
+        FactoryBot.create(:measurement, location_id: location3.id, till_date_time: 1.hour.ago)
+        expected_response = [location1, location2].to_json
+        get no_current_measurements_api_internal_locations_path
+        expect(response.body).to be_json_eql(expected_response)
+      end
     end
   end
 end
