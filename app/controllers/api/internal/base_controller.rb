ca40a281597b6ebb9540be70e79903506a6fa933
madking55 <madking55@gmail.com>
2020-03-21 16:12:37 +0100

    Remove request forgery protection

--- app/controllers/api/internal/base_controller.rb
+++ app/controllers/api/internal/base_controller.rb
@@ -2,7 +2,6 @@ module API
   module Internal
     class BaseController < ActionController::Base
       abstract!
-      protect_from_forgery with: :exception
       rescue_from(ActionController::ParameterMissing) do |parameter_missing_exception|
         error = {}
         error[parameter_missing_exception.param] = ['parameter is required']
