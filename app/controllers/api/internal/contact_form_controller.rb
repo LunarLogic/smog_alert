46855c5dd66f492856aba78afff65c5b1a44392d
madking55 <madking55@gmail.com>
2020-03-21 16:52:04 +0100

    Clean code after code review

--- app/controllers/api/internal/contact_form_controller.rb
+++ app/controllers/api/internal/contact_form_controller.rb
@@ -1,10 +1,12 @@
 class API::Internal::ContactFormController < API::Internal::BaseController
+  skip_before_action :verify_authenticity_token
+
   def create
     contact_form = ContactForm.new(contact_form_params)
     if contact_form.valid?
       email = ContactMailer.with(contact_form_params).contact_form_email
       email.deliver_now
-      render plain: email.message
+      head :no_content
     else
       render json: { errors: contact_form.errors.messages }
     end
