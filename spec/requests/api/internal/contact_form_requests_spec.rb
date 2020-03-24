9ba12815a97aea8f428e51d0aa6a7fc8ccc90045
madking55 <madking55@gmail.com>
2020-03-24 12:14:58 +0100

    Fix failing specs for contact_form controller

--- spec/requests/api/internal/contact_form_requests_spec.rb
+++ spec/requests/api/internal/contact_form_requests_spec.rb
@@ -1,41 +1,48 @@
 describe API::Internal::ContactFormController do
   describe 'POST /api/internal/contact_form' do
-    
     context 'when contact form params valid' do
-      let!(:valid_params) { { sender_name: 'Test Sender', sender_email: 'test@example.com', message: 'Test message' } }
-      let!(:contact_form) { ContactForm.new(valid_params) }
+      let!(:valid_params) do
+        {
+          contact_form:
+          { sender_name: 'Test Sender',
+            sender_email: 'test@example.com',
+            message: 'Test message' }
+        }
+      end
+
       let!(:mail) { ContactMailer.with(valid_params).contact_form_email }
 
       it 'returns 204 No Content' do
-        post '/api/internal/contact_form', params: valid_params 
-        # byebug
-        expect(response.status).to eq(204)
+        post '/api/internal/contact_form', params: valid_params
 
-        # TODO: Test fails
-        # 1) API::Internal::ContactFormController POST /api/internal/contact_form when contact form params valid returns 204 No Content
-        # Failure/Error: expect(response.status).to eq(204)
-        
-        #   expected: 204
-        #        got: 422
+        expect(response.status).to eq(204)
       end
     end
 
     context 'when contact form params NOT valid' do
-      let!(:invalid_params) { { sender_name: '', sender_email: 'test.example.com', message: 'Test message' } }
-      let!(:contact_form) { ContactForm.new(invalid_params) }
+      let!(:invalid_params) do
+        {
+          contact_form:
+          { sender_name: '',
+            sender_email: 'test.example.com',
+            message: 'Test message' }
+        }
+      end
+
+      before { post '/api/internal/contact_form', params: invalid_params }
+
+      it 'returns 200 OK status' do
+        expect(response.status).to eq(200)
+      end
 
       it 'renders json with errors messages' do
-        skip
-        post '/api/internal/contact_form', params: invalid_params 
-        # byebug
-        expect(response.status).to eq(200)
         expect(response.body).to include('errors')
-        # TODO: Test fails
-        # 2) API::Internal::ContactFormController POST /api/internal/contact_form when contact form params NOT valid renders json with errors messages
-        # Failure/Error: expect(response.status).to eq(200)
-        
-        #   expected: 200
-        #        got: 422
+        expect(response.body).to eq(
+          {
+            errors: { sender_name: ['nie może być puste'],
+                      sender_email: ['jest nieprawidłowe'] }
+          }.to_json,
+        )
       end
     end
   end
