afd36831d56c8f145b4fa6a595b19e872e08c442
madking55 <madking55@gmail.com>
2020-03-24 12:39:52 +0100

    Refactor after code review

--- spec/mailers/contact_mailer_spec.rb
+++ spec/mailers/contact_mailer_spec.rb
@@ -1,21 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe ContactMailer, type: :mailer do
-  describe 'create' do
+  describe '#contact_form_email' do
     let(:params) { { sender_name: 'Test Sender', sender_email: 'test@example.com', message: 'Test message' } }
-    let(:mail) { ContactMailer.with(params).contact_form_email }
+    subject { described_class.with(params).contact_form_email }
 
     it 'renders headers' do
-      expect(mail.from).to eq(['from@example.com'])
-      expect(mail.to).to eq(['gmina@example.com'])
-      expect(mail.subject).to eq('Question from Smog Alert')
+      expect(subject.from).to eq(['from@example.com'])
+      expect(subject.to).to eq(['gmina@example.com'])
+      expect(subject.subject).to eq('Question from Smog Alert')
     end
 
     it 'renders the body' do
-      expect(mail.body.encoded).to include(params[:sender_email])
-      expect(mail.body.encoded).to include(params[:sender_name])
-      expect(mail.body.encoded).to include(params[:message])
-      expect(mail.body.encoded).to include('This message was sent from Smog Alert contact form')
+      expect(subject.body.encoded).to include(params[:sender_email])
+      expect(subject.body.encoded).to include(params[:sender_name])
+      expect(subject.body.encoded).to include(params[:message])
+      expect(subject.body.encoded).to include('This message was sent from Smog Alert contact form')
     end
   end
 end
