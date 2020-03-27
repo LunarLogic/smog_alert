a837bab77f3427b8f8f39fe025553bf0a9cce8ad
Olga Czarnecka <olga.czarnecka@lunarlogic.io>
2020-03-27 17:18:50 +0100

    refactor contact_form to return only one validation msg regarding email field at once

--- app/services/contact_form.rb
+++ app/services/contact_form.rb
@@ -2,5 +2,5 @@ class ContactForm
   include ActiveModel::Model
   attr_accessor :sender_email, :sender_name, :message
   validates :sender_email, :sender_name, :message, presence: true
-  validates :sender_email, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
+  validates :sender_email, format: { with: /\A[^@\s]+@[^@\s]+\z/ }, allow_blank: true
 end
