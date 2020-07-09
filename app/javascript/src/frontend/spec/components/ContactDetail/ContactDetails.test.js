d92cf859cedd350ed2ec6ddd998c376cc2a2232c
Olga Czarnecka <olga.czarnecka@lunarlogic.io>
2020-07-09 16:50:15 +0200

    add react-testing-library & run demo test

--- app/javascript/src/frontend/spec/components/ContactDetail/ContactDetails.test.js
+++ app/javascript/src/frontend/spec/components/ContactDetail/ContactDetails.test.js
@@ -2,9 +2,21 @@ import React from "react";
 import { mount } from "enzyme";
 import ContactDetail from "../../../components/ContactDetail/ContactDetail";
 
+// using enzyme
 describe("<ContactDetail />", () => {
   it("displays contact detail with default icon", () => {
     const wrapper = mount(<ContactDetail item="email@gmail.com" />);
     expect(wrapper.text()).toBe("email@gmail.com");
+  });
+});
+
+import { render, screen } from "@testing-library/react";
+
+// using react-testing-library
+describe("<ContactDetail />", () => {
+  it("displays contact detail with default icon", () => {
+    render(<ContactDetail item="email@gmail.com" />);
+    screen.debug(screen.getByText("email@gmail.com"));
+    expect(screen.getByText("email@gmail.com")).toBeTruthy();
   });
 });
