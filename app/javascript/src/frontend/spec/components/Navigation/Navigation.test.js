30bfd99f8d6bc8ff4d9c1d0b4114334d739b1da0
franiakat <frania.kat@gmail.com>
2020-07-10 12:59:17 +0200

    Add rtl tests for navigation

--- app/javascript/src/frontend/spec/components/Navigation/Navigation.test.js
+++ app/javascript/src/frontend/spec/components/Navigation/Navigation.test.js
@@ -1,48 +1,70 @@
 // TODO:
 import React from "react";
-import { act } from "react-dom/test-utils";
-import { mount } from "enzyme";
+import { Router } from "react-router-dom";
+import { createMemoryHistory } from "history";
+import { render, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import App from "../../../App";
+
 import MockAdapter from "axios-mock-adapter";
 import axios from "axios";
-import { MemoryRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { Router, BrowserRouter } from "react-router-dom";
-import { createMemoryHistory } from "history";
-
-import { Navigation } from "../../../components/Navigation/Navigation";
-import App from "../../../App";
 
 import {
   mockGettingCurrentMeasurements,
   mockGettingOrganizationCurrentData,
-  mockGettingArticles
+  mockGettingArticles,
+  mockGettingCalendarValuesData,
+  mockGettingFirstMonthData,
+  mockGettingHourlyAverageForMothData,
+  mockGettingCalendarStatusData
 } from "../../helpers/mockHelpers";
 import flushPromises from "../../helpers/flushPromises";
 import { store } from "../../../redux/store";
 
 describe("Navigation component", () => {
   it("renders correct page when user chooses option from navigation", async () => {
-    // const mockAdaper = new MockAdapter(axios);
-    // mockGettingCurrentMeasurements(mockAdaper);
-    // mockGettingArticles(mockAdaper);
-    // mockGettingOrganizationCurrentData(mockAdaper);
-    // // const history = createMemoryHistory();
-    // const wrapper = mount(
-    //   <Provider store={store}>
-    //     <BrowserRouter>
-    //       <App />
-    //     </BrowserRouter>
-    //   </Provider>
-    // );
-    //
-    //     await flushPromises();
-    //     wrapper.update();
-    //     expect(wrapper.exists(".homepage")).toEqual(true);
-    //     const navLink = wrapper.find(".navigation__links-item");
-    //     // expect(navLink.at(0).hasClass("active")).toEqual(true);
-    //     act(() => {
-    //       navLink.at(0).simulate("click");
-    //     });
-    //     expect(wrapper.exists(".news")).toEqual(true);
+    const mockAdapter = new MockAdapter(axios);
+
+    mockGettingCurrentMeasurements(mockAdapter);
+    mockGettingArticles(mockAdapter);
+    mockGettingOrganizationCurrentData(mockAdapter);
+    mockGettingCalendarValuesData(mockAdapter);
+    mockGettingCalendarStatusData(mockAdapter);
+    mockGettingFirstMonthData(mockAdapter);
+    mockGettingHourlyAverageForMothData(mockAdapter);
+
+    const history = createMemoryHistory();
+
+    const { findByText, getAllByText } = render(
+      <Provider store={store}>
+        <Router history={history}>
+          <App />
+        </Router>
+      </Provider>
+    );
+
+    await flushPromises();
+
+    expect(await findByText(/aktualna jakość powietrza/i)).toBeInTheDocument();
+
+    fireEvent.click(getAllByText(/aktualności/i)[0]);
+    expect(
+      await findByText(/Consequatur quisquam reprehenderit sequi/i)
+    ).toBeInTheDocument();
+
+    fireEvent.click(getAllByText(/czym oddycham/i)[0]);
+    expect(await findByText(/obalamy mity/i)).toBeInTheDocument();
+
+    fireEvent.click(getAllByText(/rozwiązania/i)[0]);
+    expect(
+      await findByText(/sprawdź jakie kroki możesz podjąć/i)
+    ).toBeInTheDocument();
+
+    fireEvent.click(getAllByText(/statystyki/i)[0]);
+    expect(await findByText(/kalendarz/i)).toBeInTheDocument();
+
+    fireEvent.click(getAllByText(/zmień piec/i)[0]);
+    expect(await findByText(/wymień kocioł/i)).toBeInTheDocument();
   });
 });
--- app/javascript/src/frontend/spec/helpers/mockHelpers.js
+++ app/javascript/src/frontend/spec/helpers/mockHelpers.js
@@ -1,5 +1,9 @@
 import mockCities from "../__mocks__/citiesPollutionDataMock.json";
 import articlesMock from "../__mocks__/articlesMock.json";
+import calendarDailyValuesMock from "../__mocks__/calendarDailyValuesMock.json";
+import calendarStatusDataMock from "../__mocks__/calendarStatusDataMock.json";
+import chartFirstMonthDataMock from "../__mocks__/chartFirstMonthDataMock.json";
+import chartHourlyAverageForMonthData from "../__mocks__/chartHourlyAverageForMonthDataMock.json";
 
 export function mockGettingCurrentMeasurements(mockAdapter) {
   mockAdapter
@@ -19,4 +23,34 @@ export function mockGettingOrganizationCurrentData(mockAdapter) {
 
 export function mockGettingArticles(mockAdapter) {
   mockAdapter.onGet("/api/internal/articles").reply(200, articlesMock);
+}
+
+export function mockGettingCalendarValuesData(mockAdapter) {
+  mockAdapter
+    .onGet(
+      "api/internal/measurements/calendar_daily_values?date=2020-07-13&location_id=25"
+    )
+    .reply(200, calendarDailyValuesMock);
+}
+
+export function mockGettingCalendarStatusData(mockAdapter) {
+  mockAdapter
+    .onGet(
+      "/api/internal/measurements/calendar_status?year=2020&location_id=25"
+    )
+    .reply(200, { data: { ...calendarStatusDataMock } });
+}
+
+export function mockGettingFirstMonthData(mockAdapter) {
+  mockAdapter
+    .onGet("/api/internal/measurements/first_month")
+    .reply(200, chartFirstMonthDataMock);
+}
+
+export function mockGettingHourlyAverageForMothData(mockAdapter) {
+  mockAdapter
+    .onGet(
+      "/api/internal/measurements/hourly_average_for_month?date=01-6-2020&location_id=25"
+    )
+    .reply(200, chartHourlyAverageForMonthData);
 }
